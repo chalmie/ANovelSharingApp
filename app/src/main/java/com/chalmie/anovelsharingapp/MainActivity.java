3aac1b9a55761203aa0c1a5b8544b6e986fbfec0
chalmie <blairchalmers@gmail.com>
2016-04-22 12:42:49 -0700

    add MainActivity

--- app/src/main/java/com/chalmie/anovelsharingapp/MainActivity.java
+++ app/src/main/java/com/chalmie/anovelsharingapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.chalmie.anovelsharingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mStartSharingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartSharingButton = (Button) findViewById(R.id.startSharingbutton);
+        mStartSharingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
