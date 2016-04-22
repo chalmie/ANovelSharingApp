8d631903fc458d2aa6c13af15096b3582f880111
chalmie <blairchalmers@gmail.com>
2016-04-22 12:53:21 -0700

    pass validateButtonTextContent test for MainActivity

---
+++ app/src/test/java/com/chalmie/anovelsharingapp/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.chalmie.anovelsharingapp;
+
+import android.os.Build;
+import android.widget.Button;
+
+import org.apache.tools.ant.Main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by chalmie on 4/22/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateButtonTextContent() {
+        Button startSharingButton = (Button) activity.findViewById(R.id.startSharingbutton);
+        assertTrue("Start Sharing!".equals(startSharingButton.getText().toString()));
+    }
+
+}
