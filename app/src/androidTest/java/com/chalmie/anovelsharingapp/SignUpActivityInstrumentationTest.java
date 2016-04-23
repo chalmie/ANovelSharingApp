a370541e3783701d919e14dd825402ba296abf33
chalmie <blairchalmers@gmail.com>
2016-04-23 10:09:27 -0700

    add validateEditText espresso test for signUpActivity

---
+++ app/src/androidTest/java/com/chalmie/anovelsharingapp/SignUpActivityInstrumentationTest.java
@@ -0,0 +1,37 @@
+package com.chalmie.anovelsharingapp;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by chalmie on 4/23/16.
+ */
+public class SignUpActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<SignUpActivity> activityTestRule =
+            new ActivityTestRule<>(SignUpActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.usernameEditText)).perform(typeText("testUsername")).check(matches(withText("testUsername")));
+    }
+
+    @Test
+    public void usernameIsSentToProfileActivity() {
+        String username = "testUsername";
+        onView(withId(R.id.usernameEditText)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.signUpButton)).perform(click());
+        onView(withId(R.id.welcomeTextView)).check(matches(withText("Welcome " + username + "!")));
+    }
+}
