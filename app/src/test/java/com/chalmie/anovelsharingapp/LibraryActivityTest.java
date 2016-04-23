3b76cf0e5f00bb827dea1a9c67dc9f4373aadcf5
chalmie <blairchalmers@gmail.com>
2016-04-23 13:25:57 -0700

    add userLibraryListViewPopulates test for LibraryActivity

---
+++ app/src/test/java/com/chalmie/anovelsharingapp/LibraryActivityTest.java
@@ -0,0 +1,36 @@
+package com.chalmie.anovelsharingapp;
+
+import android.os.Build;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by chalmie on 4/23/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class LibraryActivityTest {
+    private LibraryActivity activity;
+    private ListView mUserLibraryListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(LibraryActivity.class);
+        mUserLibraryListView = (ListView) activity.findViewById(R.id.userLibraryListView);
+    }
+
+    @Test
+    public void userLibraryListViewPopulates() {
+        assertNotNull(mUserLibraryListView.getAdapter());
+        assertEquals(mUserLibraryListView.getAdapter().getCount(), 7);
+    }
+}
