8998f8e44dbec5592e9a62d700a8c0fcea6b492b
chalmie <blairchalmers@gmail.com>
2016-04-23 13:18:46 -0700

    add listview to LibraryActivity and populate with hardcoded book array

--- app/src/main/java/com/chalmie/anovelsharingapp/LibraryActivity.java
+++ app/src/main/java/com/chalmie/anovelsharingapp/LibraryActivity.java
@@ -2,12 +2,23 @@ package com.chalmie.anovelsharingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LibraryActivity extends AppCompatActivity {
+    @Bind(R.id.userLibraryListView) ListView mUserLibraryListView;
+    private String[] userBooks = new String[] {"A Confederacy of Dunces", "The Idiot", "Candide", "Infinite Jest", "Oliver Twist", "The Trial", "The Picture of Dorian Gray"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, userBooks);
+        mUserLibraryListView.setAdapter(adapter);
     }
 }
