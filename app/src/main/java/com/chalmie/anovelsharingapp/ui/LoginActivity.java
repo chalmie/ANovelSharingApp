62912db202fa1e086c1abab5c050b38376c79ce3
chalmie <blairchalmers@gmail.com>
2016-05-04 11:39:46 -0700

    add showErrorToast method to LoginActivity

--- app/src/main/java/com/chalmie/anovelsharingapp/ui/LoginActivity.java
+++ app/src/main/java/com/chalmie/anovelsharingapp/ui/LoginActivity.java
@@ -5,13 +5,17 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.chalmie.anovelsharingapp.Constants;
 import com.chalmie.anovelsharingapp.R;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -34,6 +38,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mSharedPreferencesEditor = mSharedPreferences.edit();
         mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
 
+        mLoginButton.setOnClickListener(this);
         mSignUpButton.setOnClickListener(this);
     }
 
@@ -59,5 +64,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (password.equals("")) {
             mPasswordEditText.setError("Please enter your password");
         }
+
+        mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                if (authData != null) {
+                    String userUid = authData.getUid();
+                    mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                switch (firebaseError.getCode()) {
+                    case FirebaseError.INVALID_EMAIL:
+                    case FirebaseError.USER_DOES_NOT_EXIST:
+                        mEmailEditText.setError("Please check that you entered your email correctly");
+                        break;
+                    case FirebaseError.INVALID_PASSWORD:
+                        mEmailEditText.setError(firebaseError.getMessage());
+                        break;
+                    case FirebaseError.NETWORK_ERROR:
+                        showErrorToast("There was a problem with the network connection");
+                        break;
+                    default:
+                        showErrorToast(firebaseError.toString());
+                }
+            }
+        });
+    }
+
+    private void showErrorToast(String message) {
+        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
