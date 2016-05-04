57203b49b7395ce271243ed7e385e4b69a05972a
chalmie <blairchalmers@gmail.com>
2016-05-04 11:08:44 -0700

    add login with password to loginActivity

--- app/src/main/java/com/chalmie/anovelsharingapp/ui/LoginActivity.java
+++ app/src/main/java/com/chalmie/anovelsharingapp/ui/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.chalmie.anovelsharingapp.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.chalmie.anovelsharingapp.Constants;
 import com.chalmie.anovelsharingapp.R;
+import com.firebase.client.Firebase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,22 +21,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.loginButton) Button mLoginButton;
     @Bind(R.id.emailEditText) EditText mEmailEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    private Firebase mFirebaseRef;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
 
         mSignUpButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        if (v == mLoginButton) {
+            loginWithPassword();
+        }
         if (v == mSignUpButton) {
             Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
+        }
+    }
+
+    public void loginWithPassword() {
+        String email = mEmailEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Please enter your password");
         }
     }
 }
