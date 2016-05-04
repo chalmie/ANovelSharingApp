0d089298c0e4af308a594f5c7064a8387b130f5c
chalmie <blairchalmers@gmail.com>
2016-05-04 10:10:06 -0700

    add user model

---
+++ app/src/main/java/com/chalmie/anovelsharingapp/models/User.java
@@ -0,0 +1,24 @@
+package com.chalmie.anovelsharingapp.models;
+
+/**
+ * Created by chalmie on 5/4/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
