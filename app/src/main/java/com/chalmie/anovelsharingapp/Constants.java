0478049a670ea8f4e9c11468b83fe4d0cf95c3f4
chalmie <blairchalmers@gmail.com>
2016-05-04 10:29:05 -0700

    update constants

--- app/src/main/java/com/chalmie/anovelsharingapp/Constants.java
+++ app/src/main/java/com/chalmie/anovelsharingapp/Constants.java
@@ -4,10 +4,21 @@ package com.chalmie.anovelsharingapp;
  * Created by chalmie on 4/26/16.
  */
 public class Constants {
-    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+    //GoogleBooksApi
     public static final String API_KEY = BuildConfig.API_KEY;
+
+    //Firebase
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
+    //Firebase UserLibrary
     public static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
     public static final String BOOK_QUERY_TITLE_PARAMETER = "q";
     public static final String FIREBASE_BOOKS = "books";
     public static final String FIREBASE_URL_ADDED_BOOK = FIREBASE_URL + FIREBASE_BOOKS;
+
+    //Firebase users
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
